218217e7d7886acfb82c7d6ae41fa70717b67366
AUTplayed <fips.hem@gmail.com>
2018-12-27 20:31:52 +0100

    simplified js

--- src/main/resources/public/scripts/main.js
+++ src/main/resources/public/scripts/main.js
@@ -3,12 +3,7 @@ var $ = document.querySelector.bind(document);
 
 async function swRegister() {
 	if ('serviceWorker' in navigator) {
-		try {
-			let serviceWorker = await navigator.serviceWorker.register('/scripts/sw.js')
-			console.log(`Service worker registered ${serviceWorker}`)
-		} catch (err) {
-			console.error(`Failed to register service worker: ${err}`)
-		}
+		let serviceWorker = await navigator.serviceWorker.register('/scripts/sw.js');
 	}
 };
 
--- src/main/resources/public/scripts/sw.js
+++ src/main/resources/public/scripts/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "V1"
+const CACHE_NAME = "V1";
 
 this.addEventListener('install', async function () {
 	const cache = await caches.open(CACHE_NAME);
@@ -7,25 +7,20 @@ this.addEventListener('install', async function () {
 		'/styles/global.css',
 		'/styles/index.css',
 		'/scripts/main.js',
-	])
-})
+		'/scripts/show.js'
+	]);
+});
 
 this.addEventListener('fetch', event => {
 	event.respondWith(async () => {
-		try {
-			//Try to get the cached response
-			const cachedResponse = await caches.match(event.request)
-			if (cachedResponse) {
-				//Return the cached response if present
-				console.log(`Cached response ${cachedResponse}`)
-				return cachedResponse
-			}
-			//Get the network response if no cached response is present
-			const netResponse = await fetch(event.request)
-			return netResponse
-		} catch (err) {
-			console.error(`Error ${err}`)
-			throw err
+		//Try to get the cached response
+		const cachedResponse = await caches.match(event.request);
+		if (cachedResponse) {
+			//Return the cached response if present
+			return cachedResponse
 		}
+		//Get the network response if no cached response is present
+		const netResponse = await fetch(event.request)
+		return netResponse
 	})
 })
